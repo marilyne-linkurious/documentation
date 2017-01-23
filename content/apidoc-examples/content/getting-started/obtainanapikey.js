11bc6427bff631f46100fd5ef62f4df25f65c0da
Francesco Infante <fra.inf@icloud.com>
2017-01-23 17:58:25 +0100

    obtain api key example

--- content/apidoc-examples/content/getting-started/obtainanapikey.js
+++ content/apidoc-examples/content/getting-started/obtainanapikey.js
@@ -1,5 +1,15 @@
+// we need to be admin to obtain a new API key
 request.post({
-  uri: 'http://crunchbase.linkurio.us/api/admin/applications'
+  uri: 'http://crunchbase.linkurio.us/api/auth/login',
+  body: {
+    usernameOrEmail: 'admin',
+    password: 'demo_admin'
+  }
 }, function(err, res) {
-  console.log(res.body);
+
+  request.post({
+    uri: 'http://crunchbase.linkurio.us/api/admin/applications'
+  }, function(err, res) {
+    console.log(res.body);
+  });
 });
